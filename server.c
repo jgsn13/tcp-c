f6b44525fb65c1db12609bf79964633cd60127e3
Joaquim Gregório da Silva Neto <joaquim.ibi@gmail.com>
2022-08-21 20:29:13 -0300

    feat: add driver function to handle server creation and closing

--- server.c
+++ server.c
@@ -9,6 +9,9 @@
 #define PORT 8080
 #define SA struct sockaddr
 
+/**
+ * Function designed for chat between client and server
+ */
 void clientServerChat(int connfd) {
     char buff[MAX];
     int n;
@@ -39,4 +42,61 @@ void clientServerChat(int connfd) {
     }
 }
 
-int main() { return 0; }
+/**
+ * Driver function
+ */
+int main() {
+    int sockfd, connfd, len;
+    struct sockaddr_in servaddr, cli;
+
+    // socket creation and verification
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        printf("socket creation failed...\n");
+        exit(0);
+    } else {
+        printf("Socket successfully created...\n");
+    }
+
+    bzero(&servaddr, sizeof(servaddr));
+
+    // assign IP, PORT
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(PORT);
+
+    // Binding newly created socket to given IP and verification
+    if ((bind(sockfd, (SA *)&servaddr, sizeof(servaddr))) != 0) {
+        printf("socket bind failed...\n");
+        exit(0);
+    } else {
+        printf("Socket successfully binded...\n");
+    }
+
+    // Now server is ready to listen and verification
+    if ((listen(sockfd, 5)) != 0) {
+        printf("Listen failed...\n");
+        exit(0);
+    } else {
+        printf("Server listening...\n");
+    }
+
+    len = sizeof(cli);
+
+    // Accept the data packet from client and verification
+    connfd = accept(sockfd, (SA *)&cli, &len);
+    if (connfd < 0) {
+        printf("server accept failed...\n");
+        exit(0);
+    } else {
+        printf("server accept the client...\n");
+    }
+
+    // function for chatting between client and server
+    clientServerChat(connfd);
+
+    // after chatting close the socket
+    close(sockfd);
+
+    return 0;
+}
